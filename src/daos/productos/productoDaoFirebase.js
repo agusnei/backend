7ba84696d1166b31eab88c91762f98c57f544efd
agusnei <86327876+agusnei@users.noreply.github.com>
2022-07-28 20:46:13 -0300

    Delete src directory

--- src/daos/productos/productoDaoFirebase.js
+++
@@ -1,9 +0,0 @@
-import ContenedorFirebase from '../../contenedores/ContenedorFirebase.js';
-
-class ProductoDaoFirebase extends ContenedorFirebase {
-    constructor() {
-        super('productos');
-    }
-}
-
-export default ProductoDaoFirebase;
